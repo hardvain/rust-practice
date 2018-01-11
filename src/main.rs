dda78d0b5e4c7546c41d424bf1db62761a69fbec
Aravindh Sridharan <mail@aravindh.io>
2018-01-11 21:00:43 +0100

    Added some combinations and some code for it

--- src/main.rs
+++ src/main.rs
@@ -1,64 +1,79 @@
+struct Sample {
+  key: i32,
+}
+impl Sample {
+  pub fn owned(self) {}
+  pub fn shared_ref(&self) {}
+  pub fn mutable_ref(&self) {}
+}
 fn main() {
-
-  // 
-  let _unused_integer = 0;
-  let copyable_integer = 5;
-  let copied_from_copyable_integer = copyable_integer;
-  println!("copyable_integer can be used here: {}", copyable_integer);
-  println!("copied_from_copyable_integer can also be used here: {}", copied_from_copyable_integer);
-  let incremented_copyable_integer = copyable_integer + 1;
-  println!("incremented_copyable_integer : {}", incremented_copyable_integer);
-  println!("copyable_integer still has the same value: {}", copyable_integer);
-  
-  // copyable_integer can be re defined which will shadow the old definition
-  let copyable_integer = 4;
-  // copyable_integer cannot just be re assigned alone because bindings are mutable by default
-  // copyable_integer = 1;
-
-  // Effect of scopes on copyable values
-  let root_scoped_value = 1;
   {
-    println!("root scoped value inside beginning of first inner scope {}", root_scoped_value);
-    let mut first_inner_scoped_value = root_scoped_value;
-    println!("root scoped value after assignment to another binding {}", root_scoped_value);
-    println!("first_inner_scoped_value {}", first_inner_scoped_value);
-    first_inner_scoped_value = first_inner_scoped_value + 1;
-    println!("root scoped value after mutation to assigned binding {}", root_scoped_value);
-    println!("first_inner_scoped_value after mutation {}", first_inner_scoped_value);
+    {
+      let sample = Sample { key: 1 };
+      sample.owned();
+      // cannot use below calls because value has been moved
+      // sample.owned();
+      // sample.shared_ref();
+      // sample.mutable_ref();
+    }
+    {
+      let sample = Sample { key: 1 };
+      sample.shared_ref();
+      sample.shared_ref();
+      sample.mutable_ref();
+      sample.mutable_ref();
+      sample.owned();
+      // sample.owned();
+    }
+    {
+      let sample = Sample { key: 1 };
+      sample.shared_ref();
+      sample.owned();
+      // cannot call below because value has been moved
+      // sample.mutable_ref();
+    }
+    {
+      let sample = Sample { key: 1 };
+      sample.mutable_ref();
+      sample.mutable_ref();
+      sample.shared_ref();
+      sample.shared_ref();
+      sample.owned();
+    }
   }
 
-  println!("root scoped value at root scope {}", root_scoped_value);
-
-  // Effect of immutable bindings on passing to functions
-
+  {
+    {
+      let mut sample = Sample { key: 1 };
+      sample.owned();
+      // cannot use below calls because value has been moved
+      // sample.owned();
+      // sample.shared_ref();
+      // sample.mutable_ref();
+    }
+    {
+      let mut sample = Sample { key: 1 };
+      sample.shared_ref();
+      sample.shared_ref();
+      sample.mutable_ref();
+      sample.mutable_ref();
+      sample.owned();
+      // sample.owned();
+    }
+    {
+      let mut sample = Sample { key: 1 };
+      sample.shared_ref();
+      sample.owned();
+      // cannot call below because value has been moved
+      // sample.mutable_ref();
+    }
+    {
+      let mut sample = Sample { key: 1 };
+      sample.mutable_ref();
+      sample.mutable_ref();
+      sample.shared_ref();
+      sample.shared_ref();
+      sample.owned();
+    }
+  }
 }
-
-/*
-  1. Bindings
-    1. Immutable Bindings
-    2. Mutable Bindings
-  2. Scopes
-    1. Current Scope
-    2. Inner Scope
-    3. Outer Scope
-  3. Calling Member functions on variables
-    1. Functions that accept by value
-    2. Functions that accept by shared reference
-    3. Functions that accept by mutable reference
-  4. Calling Functions on
-    1. Owned values
-      1. That accept by value
-      2. That accept by shared reference
-      3. That accept by mutable reference
-    2. Shared Reference values
-      1. That accept by value
-      2. That accept by shared reference
-      3. That accept by mutable reference
-    3. Mutable reference values
-      1. That accept by value
-      2. That accept by shared reference
-      3. That accept by mutable reference
-  5. Closures
-  6. Pattern Matching
-  7. Lifetimes
-*/
